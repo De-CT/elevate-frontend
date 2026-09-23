a3fbf7697ee152d89b74f0a5ff50db8a6f02f312
cas1908 <schima05@gmail.com>
2026-09-24 00:53:27 +0100

    added the dashboard page

---
+++ src/app/(member)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div className="">
+            <h1 className="text-4xl text-center">Here is the dashboard</h1>
+        </div>
+    )
+}
+
+export default Dashboard
